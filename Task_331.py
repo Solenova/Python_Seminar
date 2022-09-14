ef15bd311386245ef2bfd501c2090ae450cb435c
OlgaS <roga&kopita@mail.ru>
2022-09-14 23:51:33 +0500

    update

--- Task_331.py
+++ Task_331.py
@@ -4,7 +4,14 @@
 # Пример:
 # - [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
 
-list=[2, 3, 5, 9, 3]
+import random
+col=int(input('введите количество элементов списка '))
+list=[]
+for i in range(col):
+    temp=random.randint(0,10)
+    list.append(temp)
+print (list)
+
 summ=0
 for i in range(len(list)):
     if i%2!=0:
