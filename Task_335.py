649c66ac8d824ff1a5991587944a1f17d3e90dbb
OlgaS <roga&kopita@mail.ru>
2022-09-25 22:52:11 +0500

    список чисел Фибоначчи, в том числе для отрицательных индексов optim

--- Task_335.py
+++ Task_335.py
@@ -7,12 +7,15 @@ def fib(n):
     if n == 0:
         return 0
     elif n == 1 or n==-1:
-            return 1
+        return 1
     elif n >1:
         return fib(n-1) + fib(n-2)
     if n < -1:
         return fib(n+2)-fib(n+1)
 list=[]
-for e in range(-num,num+1):
-    list.append(fib(e))
-print (list)    
+# for e in range(-num,num+1):
+#     list.append(fib(e))
+# print (list)    
+
+# Семинар 6 улучшение кода
+print ([fib(i) for i in range (-num,num+1) ])
